579eace1c41121028be2df64f9f2d808a35fbf3a
alfredo-sampron <alfredo.sampron@despegar.com>
2023-12-06 15:32:16 -0300

    create params.py

---
+++ notebooks/scripts/params.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+
+def get_params():
+    with open(os.path.join("..", "notebooks", "using.txt"), 'r') as file:
+        config_file = file.read()
+
+    if not config_file.endswith('.yaml'):
+         config_file = config_file + '.yaml'
+
+    with open(os.path.join("..", "notebooks", config_file), 'r') as file:
+        yaml_data = yaml.safe_load(file)
+
+    tablename_prefix = yaml_data['tablename_prefix']
+    index = yaml_data['index']
+    start_train = yaml_data['start_train']
+    end_test = yaml_data['end_test']
+    yaml_data['tablename'] = f"""{tablename_prefix}_{index}_{start_train.replace("-","_")}__{end_test.replace("-","_")}"""
+    
+    return yaml_data
