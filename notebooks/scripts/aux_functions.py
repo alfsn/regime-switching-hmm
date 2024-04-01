6c594ef8574c7bbc0d8767753ba015b3f320e6f0
alfredo-sampron <alfredo.sampron@despegar.com>
2024-04-01 14:59:21 -0300

    clean_modelname as aux

--- notebooks/scripts/aux_functions.py
+++ notebooks/scripts/aux_functions.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+import pandas as pd
+
 
 def generate_columns(stock: str, contains_vol: bool, contains_USD: bool):
     """
@@ -22,8 +24,14 @@ def generate_columns(stock: str, contains_vol: bool, contains_USD: bool):
     return columns
 
 
-
-def save_as_pickle(data, resultsroute:str, model_type:str, tablename:str, criterion: str, type_save: str):    
+def save_as_pickle(
+    data,
+    resultsroute: str,
+    model_type: str,
+    tablename: str,
+    criterion: str,
+    type_save: str,
+):
     """
     Saves data as a pickle file with a descriptive filename based on the provided parameters.
     """
@@ -35,9 +43,9 @@ def save_as_pickle(data, resultsroute:str, model_type:str, tablename:str, criter
         "wb",
     ) as output_file:
         pickle.dump(data, output_file)
-        
-        
-def get_all_results_matching(resultsroute:str, substrings:list):
+
+
+def get_all_results_matching(resultsroute: str, substrings: list):
     """
     Finds all pickle files in a directory that contain all of the provided substrings in their filenames.
 
@@ -49,13 +57,41 @@ def get_all_results_matching(resultsroute:str, substrings:list):
 
     for filename in os.listdir(resultsroute):
         file_path = os.path.join(resultsroute, filename)
-        coincidences=0
+        coincidences = 0
         if os.path.isfile(file_path):
             for substring in substrings:
                 if substring in filename:
-                    coincidences+=1
+                    coincidences += 1
             if coincidences == len(substrings):
                 all_results[filename] = file_path
 
     print(all_results)
     return all_results
+
+
+def subset_of_columns(df: pd.DataFrame, substring: str, exclude: str = None):
+    """
+    Filters a DataFrame to include only columns containing a given substring,
+    optionally excluding columns containing another substring.
+
+    Returns:
+    pd.DataFrame: A new DataFrame containing only the filtered columns.
+    """
+    filtered_columns = [col for col in df.columns if substring in col]
+
+    if exclude is not None:
+        filtered_columns = [
+            col for col in filtered_columns.copy() if exclude not in col
+        ]
+
+    return df[filtered_columns]
+
+
+def clean_modelname(name: str, substring_to_replace: str, tablename: str):
+    name.replace(f"{substring_to_replace}", ""
+                 ).replace(".pickle", ""
+                           ).replace("best", ""
+                                     ).replace(tablename, ""
+                                               ).replace("__", "_"
+                                                         ).replace("__", "_")
+    return name
